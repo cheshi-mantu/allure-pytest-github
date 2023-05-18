de8639b2b6d46c2b55e75a215914f9be18fa13e0
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-05-18 15:48:06 +0300

    add template

---
+++ test/authentication_test.py
@@ -0,0 +1,7 @@
+import allure
+import pytest
+from .steps import rest_steps as steps
+from .marks import microservice, layer, owner, tm4j, jira_issues
+
+def test_should_authenticate_user_with_email():
+    pass
