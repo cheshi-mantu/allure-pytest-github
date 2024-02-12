de897b1b9f03b0444cbe220bc3bac1f01016ccaf
Egor Ivanov <cheshi.mantu@protonmail.com>
2024-02-12 21:16:58 +0200

    removed master branch trigger
    Signed-off-by: Egor Ivanov <cheshi.mantu@protonmail.com>

--- test/authentication_test.py
+++ test/authentication_test.py
@@ -3,13 +3,6 @@ import pytest
 from .steps import rest_steps as steps
 from .marks import microservice, layer, owner, tm4j, jira_issues
 
-@allure.id("187698")
-@allure.title("Successful auth with username and password")
-@allure.tag("critical", "regress", "smoke")
-@allure.story("Built-in auth")
-@allure.feature("Authentication")
-@allure.label("msrv", "UAA")
-@allure.label("owner", "egorivanov")
 def test_should_authenticate_user_with_email():
     with allure.step("Open main page"):
         pass
