73a38bdc78e255be007dd2e8ad8acbb201aa9596
Egor I <egor.ivanov@pm.me>
2025-09-19 12:33:45 +0200

    upd circle pipeline config
    Signed-off-by: Egor I <egor.ivanov@pm.me>

--- test/project-e2e-tests.py
+++ test/project-e2e-tests.py
@@ -3,11 +3,19 @@ import pytest
 from .steps import web_steps as steps
 from .marks import layer, owner
 
-
 pytestmark = [
-    layer("none"),
-    owner("egorivanov"),
+    layer("api"),
+    owner("bugsbunny"),
 ]
 
 def test_should_project():
-    
+    with allure.step("Preconditions"):
+        with allure.step(
+                "Given a user is logged in\n"
+                "When they navigate to the dashboard\n"
+                "Then they should see their account details"
+        ):
+            pass
+    with allure.step(""):
+        print("then some actions")
+        pass
